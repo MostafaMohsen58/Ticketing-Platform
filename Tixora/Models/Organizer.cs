2f491676a8215deca86a2467a49c9f9a7d645876
fatma <fatmaabdelhamid909@gmail.com>
2025-04-24 02:49:56 +0200

    Add TicketCategory and Organizer Controllers

--- Tixora/Models/Organizer.cs
+++ Tixora/Models/Organizer.cs
@@ -1,19 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿
+using System.ComponentModel.DataAnnotations;
 using Tixora.Validation;
 
 namespace Tixora.Models
 {
     public class Organizer
     {
+        public Organizer()
+        {
+            Events = new List<Event>();
+        }
+
         public int Id { get; set; }
-        [Required(ErrorMessage = "name is required")]
+
+        [Required(ErrorMessage = "Name is required")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters")]
         [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Display(Name = "Contact Email")]
         public string ContactEmail { get; set; }
-        [PhoneNumberValidation(ErrorMessage = "phone number must be starts with \"01\" in addition to having 11 digits. ")]
+
+        [PhoneNumberValidation(ErrorMessage = "Phone number must start with \"01\" and be 11 digits long")]
+        [Display(Name = "Contact Phone")]
         public string ContactPhone { get; set; }
+
+        [Display(Name = "Logo URL")]
         public string? LogoUrl { get; set; }
+
         public virtual List<Event> Events { get; set; }
     }
 }
---
+++ Tixora/Services/Interfaces/IOrganizerService.cs
@@ -0,0 +1,13 @@
+﻿using Tixora.Models;
+
+namespace Tixora.Services.Interfaces
+{
+    public interface IOrganizerService
+    {
+        Organizer GetById(int id);
+        List<Organizer> GetAll();
+        void Add(Organizer organizer);
+        void Update(Organizer organizer);
+        void Delete(int id);
+    }
+}
